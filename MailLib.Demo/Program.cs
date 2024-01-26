88022cd533d48a76199e62ec75dbc571a52bf73d
Ahmed Abdelhamid <ahmed_a.elhamid@hotmail.com>
2024-01-26 12:11:12 +0200

    Enhanced the demo examples.

--- MailLib.Demo/Program.cs
+++ MailLib.Demo/Program.cs
@@ -9,69 +9,56 @@ using Microsoft.Extensions.Options;
 var options = new SmtpConfiguration("smtp.gmail.com", 587,
     "user@gmail.com", "user@gmail.com", "app-password");
 
-var smtpConfiguration = Options.Create(options);
-var emailService = new SmtpEmailService(smtpConfiguration);
-
-const string subject = "Welcome To Application";
-
-
-const string templateName = "invite-user.html";
-var htmlBody = HtmlTemplateHelper.ExtractStringFromHtml(GetTemplatePath(templateName));
-
-List<TemplatePlaceholder> placeholders =
-[
-    new TemplatePlaceholder { Placeholder = "{user-name}", Value = "User" }
-];
-
-var mailResources = new MailResources
-{
-    LinkedResources = new List<MailLinkedResource>
-    {
-        new()
-        {
-            ContentPath = GetAttachmentPath("app-logo.png"),
-            ContentId = "app-logo.png"
-        },
-        new()
-        {
-            ContentPath = GetAttachmentPath("arrow.png"),
-            ContentId = "arrow.png"
-        }
-    }
-};
-
-// This is an example of sending a single email to a single recipient
-
-var user = new EmailAddressModel("yuknigukko@gufum.com", "yuknigukko");
-
-var singleRecipientOptions = new SingleEmailOptions(
-    user,
-    subject,
-    htmlBody,
-    placeholders,
-    mailResources);
-
-await emailService.SendEmail(singleRecipientOptions);
-
-// This is an example of sending a single email to multiple recipients
-
 var users = new List<EmailAddressModel>
 {
     new("yuknigukko@gufum.com", "yuknigukko"),
     new("hirom41547@grassdev.com", "hirom41547"),
 };
 
+var smtpConfiguration = Options.Create(options);
+var emailService = new SmtpEmailService(smtpConfiguration);
+
+const string subject = "Welcome To Application";
+const string templateName = "invite-user.html";
+var htmlBody = HtmlTemplateHelper.ExtractStringFromHtml(GetTemplatePath(templateName));
+var mailResources = GetMailResources();
+
+// This is an example of sending a single email to a single recipient
+
+#region SingleEmailOptions
+
+var user = users.First();
+
+var singleRecipientOptions = new SingleEmailOptions(
+    user,
+    subject,
+    htmlBody,
+    [new TemplatePlaceholder { Placeholder = "{user-name}", Value = user.Name }],
+    mailResources);
+
+await emailService.SendEmail(singleRecipientOptions);
+
+#endregion
+
+// This is an example of sending a single email to multiple recipients
+
+#region SingleEmailToMultipleRecipientsOptions
+
 var multipleRecipientsOptions = new SingleEmailToMultipleRecipientsOptions(
     users,
     subject,
     htmlBody,
-    placeholders,
+    [new TemplatePlaceholder { Placeholder = "{user-name}", Value = "User" }],
     mailResources
 );
 
 await emailService.SendEmail(multipleRecipientsOptions);
 
-// This is a more advanced example of sending emails to multiple recipients
+#endregion
+
+// This is a more advanced example of sending emails to multiple recipients with user specific placeholders
+
+#region UserSpecificEmailBodyToMultipleRecipientsOptions
 
 var userSpecificEmailBodyToMultipleRecipientsOptions = new UserSpecificEmailBodyToMultipleRecipientsOptions(
     users,
@@ -83,6 +70,8 @@ var userSpecificEmailBodyToMultipleRecipientsOptions = new UserSpecificEmailBody
 
 await emailService.SendEmail(userSpecificEmailBodyToMultipleRecipientsOptions);
 
+#endregion
+
 return;
 
 static string GetTemplatePath(string templateName)
@@ -91,3 +80,22 @@ static string GetTemplatePath(string templateName)
 static string GetAttachmentPath(
     string attachmentName) =>
     Path.Combine(Directory.GetCurrentDirectory(), "wwwroot", "EmailTemplates", "Attachments", attachmentName);
+
+
+static MailResources GetMailResources()
+    => new()
+    {
+        LinkedResources =
+        [
+            new MailLinkedResource
+            {
+                ContentPath = GetAttachmentPath("app-logo.png"),
+                ContentId = "app-logo.png"
+            },
+            new MailLinkedResource
+            {
+                ContentPath = GetAttachmentPath("arrow.png"),
+                ContentId = "arrow.png"
+            }
+        ]
+    };
